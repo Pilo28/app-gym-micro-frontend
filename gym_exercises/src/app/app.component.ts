96c41e1dd4a9e3fa128b620624d72596e6d8468d
Pilo28 <pdefelitto@gmail.com>
2024-08-27 11:04:41 +0200

    added form list exercise

--- gym_exercises/src/app/app.component.ts
+++ gym_exercises/src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { ExercisesService } from "./service/exercises.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'gym_exercises';
+  private formBuilder = inject(NonNullableFormBuilder);
+  private exerciseService = inject(ExercisesService);
+  exerciseList$ = this.exerciseService.getExercises();
+
+  public entryForm = this.formBuilder.group({
+    description: ['', Validators.required],
+  });
+
+  newExercise() {
+    if (this.entryForm.valid) {
+      const newExercise = { ...this.entryForm.value };
+      this.exerciseService
+        .addExercises(newExercise)
+        .subscribe(
+          (_) => (this.exerciseList$ = this.exerciseService.getExercises())
+        );
+    }
+  }
 }
